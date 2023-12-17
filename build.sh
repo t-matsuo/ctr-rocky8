8144a78c2270248fd88c60cace5923aacb55d8d2
Takatoshi MATSUO <matsuo.tak@gmail.com>
2023-12-17 23:25:57 +0900

    support podman

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+which docker > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    BUILDER="podman"
+fi
+echo "building image using $BUIDER"
+
 ./cocker -m Dockerfile.split > _Dockerfile
-docker build -t tmatsuo/rocky8 -f _Dockerfile .
+$BUILDER build -t tmatsuo/rocky8 -f _Dockerfile .
