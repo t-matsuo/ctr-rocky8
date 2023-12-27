684d3b66dcb523224ff85f09d26f18890f9ec845
Takatoshi MATSUO <matsuo.tak@gmail.com>
2023-12-27 23:28:17 +0900

    Support BUILDKIT

--- build.sh
+++ build.sh
@@ -6,5 +6,14 @@ if [ $? -ne 0 ]; then
 fi
 echo "building image using $BUIDER"
 
-./cocker -m Dockerfile.split > _Dockerfile
+CC_OPTION="-m"
+if [ "$MODE" = "dev" ]; then
+    CC_OPTION="-c"
+    IS_DEV="-dev"
+    sed 's|^RUN|RUN --mount=type=cache,id=rocky8,target=/var/cache/dnf --mount=type=cache,id=rocky8,target=/var/lib/dnf --mount=type=cache,id=rocky8,target=/root/.cache --mount=type=cache,id=rocky8,target=/root/.npm|g' Dockerfile.split > _Dockerfile.split.tmp
+else
+    sed 's|^#__BULDKIT_MARKER__||g' Dockerfile.split > _Dockerfile.split.tmp
+fi
+
+./cocker $CC_OPTION _Dockerfile.split.tmp > _Dockerfile
 $BUILDER build -t tmatsuo/rocky8 -f _Dockerfile .
